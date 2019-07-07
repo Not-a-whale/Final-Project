da62eb444ccf89a7dbb24a7861498c6181330656
Nikita <sabaoth@ukr.net>
2019-07-07 14:11:43 +0300

    modal completed

--- js/main.js
+++ js/main.js
@@ -6,7 +6,33 @@ $( document ).ready(function() {
             $(".banner").css('margin-top', 10 + 'rem');
         }
     });
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 40) {
+            $("#toTopBtn").fadeIn();
+        } else {
+            $("#toTopBtn").fadeOut();
+        }
+    });
+
     $("#toTopBtn").click(function() {
         $('html , body').animate({scrollTop: 0}, 800, );
+    });
+
+
+    $("#login").click(function() {
+        $(".modal").show();
     })
+
+    //Close modal
+
+    $(window).click(function(e) {
+        if(e.target.className === "modal" || e.target.id === "signInButton") {
+            $(".modal").hide();
+        }
+    })
+
+    $('.slider').slick({
+    });
 });
+
